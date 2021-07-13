07f9207ab06770d1e8625ea8ab0172d9734a5bcb
Joel Turner <jturner421@gmail.com>
2021-07-13 14:52:00 -0500

    Added Alembic support

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
-from flask_cors import CORS
-from api.extensions import db
+
 from api.config import DevelopmentConfig
+from api.extensions import db, migrate
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
-    # db.init_app(app)
+    db.init_app(app)
     from api.data import all_models
-    # db.create_all()
+    migrate.init_app(app, db, render_as_batch=True)
+
     with app.app_context():
         register_blueprints(app)
+        # db.create_all()
 
     @app.shell_context_processor  # Decorator that returns items into a Flask shell session
     def shell_context():
